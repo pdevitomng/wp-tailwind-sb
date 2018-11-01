9fbc5a2a7c71f1fa34aaf4b585cf0a4bdb14729c
Paul D <pdevito@mnglabs.com>
2018-10-31 21:26:32 -0400

    partial searchbar, contact us page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,6 +18,7 @@
             <!-- 
             1) https://github.com/dukestreetstudio/zondicons/tree/master/src
             2) click on one and select 'raw'
+            detailed svg: https://github.com/sschoger/zondicons/tree/master/zondicons
           -->
             <svg class="fill-current inline-block h-4 w-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M6 4H5a1 1 0 1 1 0-2h11V1a1 1 0 0 0-1-1H4a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V5a1 1 0 0 0-1-1h-7v8l-2-2-2 2V4z"/></svg>
             Reports
--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,89 +1,97 @@
 <template>
-    <div>
-      <navbar></navbar>
-      <div role="filter-row" class="flex flex-wrap uppercase text-2xl select-none ml-16 pt-12">
-        <!-- <div :selected="true"  class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">All</div>
-        <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">On Hold</div>
-        <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">In The Lab</div>
-        <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">Complete</div>
-        <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">Rejected/Cancelled</div> -->
-        <div v-for="status in StatusOptions" :key=status.index  class="mx-4" :class="[status.name == selectedStatus ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']" @click= "selectedStatus=status.name">{{status.name}}</div>
-        <!-- <div 
-          v-for="status in StatusOptions" 
-          :key=status.index 
-          class="mx-4" 
-          @click= "selectedStatus=status.name">
-            {{status.name}}
-        </div> -->
-      </div>
-
-      <div role="search-bar" class="flex items-center pt-12 px-6">
-        <div class="ml-8 p-2">
-          <svg class="fill-current inline-block h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
-        </div>
-        <!-- <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Search by name, date of birth, identifier, physician, or test code"> -->
-        <input class="appearance-none border-b border-grey-light w-full py-2 text-grey-darker leading-tight focus:outline-none" type="text" placeholder="Search by name, date of birth, identifier, physician, or test code">
-      </div>
-
-      <div class="py-6 px-4 font-bold">This will be the filter val: {{selectedStatus}}</div>
-
-      <div role="table" class = "container mx-auto py-6">
-      <table class="container mx-auto border-2 rounded-lg shadow py-6">
-          <tr class = "border-y-2">
-          <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2">Name</th>
-          <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2">Status</th>
-          </tr>
-          <tr class = "border-t-2 border-y-2 py-4">
-          <td class="py-4 px-4">
-              <div class="text-lg font-semibold pb-1">Paul DeVito</div>
-              <div class="text-xs text-grey-dark">06/18/1991</div>
-          </td>
-          <td class="border-t-2 px-2 text-center w-1/6">
-              <div class="inline-block px-4 rounded-full bg-green-light text-green-darker font-semibold text-sm py-1">
-              Complete
-              </div>
-          </td>
-          </tr>
-      </table>
-      </div>
-
-      <div role="table" class = "container mx-auto py-6">
-      <table class="border-2 shadow py-6">
-          <tr class = "border-y-2">
-          <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">Name</th>
-          <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">DOB</th>
-          <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">Status</th>
-          </tr>
-          <tr class = "border-t-2 border-y-2 py-4">
-          <td class="py-4 px-4">
-              <div class="">Paul DeVito</div>
-          </td>
-          <td>
-              <div class="">06/18/1991</div>
-          </td>
-          <td class="border-t-2 px-2  w-1/6">
-              <div class="inline-flex px-4 rounded-full bg-green-light text-green-darker font-semibold text-sm py-1">
-              Complete
-              </div>
-          </td>
-          </tr>
-      </table>
-      </div>
-      <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      </div>
-      <router-view/> -->
+  <div>
+    <navbar></navbar>
+    <div role="filter-row" class="flex flex-wrap uppercase text-2xl select-none ml-12 pt-12">
+      <!-- <div :selected="true"  class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">All</div>
+      <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">On Hold</div>
+      <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">In The Lab</div>
+      <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">Complete</div>
+      <div :selected="false" class="mx-4" :class="[selected ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']">Rejected/Cancelled</div> -->
+      <div v-for="status in StatusOptions" 
+        :key=status.index  
+        class="mx-4" 
+        :class="[status.name == selectedStatus ? 'font-medium border-b-2 border-green' : 'hover:cursor-pointer hover:opacity-50']" 
+        @click= "selectedStatus=status.name">{{status.name}}</div>
+      <!-- <div 
+        v-for="status in StatusOptions" 
+        :key=status.index 
+        class="mx-4" 
+        @click= "selectedStatus=status.name">
+          {{status.name}}
+      </div> -->
     </div>
+
+    <div role="search-bar" class="pt-8 pb-4 px-4">
+      <search-bar @search="searchVal" search-placeholder = "Search by name, date of birth, identifier, physician, or test code"></search-bar>
+      <!-- <div class="p-2">
+        <svg class="fill-current inline-block h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
+      </div> -->
+      <!-- <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Search by name, date of birth, identifier, physician, or test code"> -->
+      <!-- <input class="appearance-none border-b border-grey-light w-full py-2 text-grey-darker leading-tight focus:outline-none" type="text" placeholder="Search by name, date of birth, identifier, physician, or test code"> -->
+    </div>
+
+    <div class="py-6 px-4 font-bold">This will be the search val: {{searchVal}}</div>
+    <div class="py-6 px-4 font-bold">This will be the filter val: {{selectedStatus}}</div>
+
+    <div role="table" class = "container mx-auto py-6">
+    <table class="container mx-auto border-2 rounded-lg shadow py-6">
+        <tr class = "border-y-2">
+        <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2">Name</th>
+        <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2">Status</th>
+        </tr>
+        <tr class = "border-t-2 border-y-2 py-4">
+        <td class="py-4 px-4">
+            <div class="text-lg font-semibold pb-1">Paul DeVito</div>
+            <div class="text-xs text-grey-dark">06/18/1991</div>
+        </td>
+        <td class="border-t-2 px-2 text-center w-1/6">
+            <div class="inline-block px-4 rounded-full bg-green-light text-green-darker font-semibold text-sm py-1">
+            Complete
+            </div>
+        </td>
+        </tr>
+    </table>
+    </div>
+
+    <div role="table" class = "container mx-auto py-6">
+    <table class="border-2 shadow py-6">
+      <tr class = "border-y-2">
+        <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">Name</th>
+        <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">DOB</th>
+        <th class="text-left uppercase bg-grey-lighter font-semibold font-grey-dark py-2 px-2">Status</th>
+      </tr>
+      <tr class = "border-t-2 border-y-2 py-4">
+        <td class="py-4 px-4">
+          <div class="">Paul DeVito</div>
+        </td>
+        <td>
+          <div class="">06/18/1991</div>
+        </td>
+        <td class="border-t-2 px-2  w-1/6">
+          <div class="inline-flex px-4 rounded-full bg-green-light text-green-darker font-semibold text-sm py-1">
+            Complete
+          </div>
+        </td>
+      </tr>
+    </table>
+    </div>
+    <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import SearchBar from "@/components/SearchBar";
 
 export default {
     name: "reports",
     components: {
-      Navbar
+      Navbar,
+      SearchBar
     },
     data() {
     return {
